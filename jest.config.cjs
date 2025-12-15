c21cc6a806bb9d723108f1af69c0f1bf63ca0810
Deotyma <kalistaewa@gmail.com>
2025-12-15 12:13:26 +0100

    libraty jest added

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/test/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+};
